c7259326c437a50febfb8e1b57b813b5c4b88a1e
SaidOs12 <deislersaidpros@ufps.edu.co>
2025-03-03 12:01:12 -0500

    Aca

--- js/app.js
+++ js/app.js
@@ -1,95 +1,43 @@
-document.addEventListener('DOMContentLoaded', () => {
+
+
     // Sample student data
-    const students = [
-        {
-            name: 'Carlos Rene Angarita Sanguino',
-            id: '05372',
-            email: 'carlosreneas@ufps.edu.co',
-            github: 'GitHub',
-            image: 'https://via.placeholder.com/300'
-        },
-        {
-            name: 'Yan Carlo Angarita Sanguino',
-            id: '00001',
-            email: 'yancarlo120b@gmail.com',
-            github: 'GitHub',
-            image: 'https://via.placeholder.com/300'
-        },
-        {
-            name: 'Claudia Yamile Gomez Llanez',
-            id: '05096',
-            email: 'claudiaygomez@ufps.edu.co',
-            github: 'GitHub',
-            image: 'https://via.placeholder.com/300'
-        },
-        {
-            name: 'Claudia Yamile Gomez Llanez',
-            id: '05096',
-            email: 'claudiaygomez@ufps.edu.co',
-            github: 'GitHub',
-            image: 'https://via.placeholder.com/300'
-        },
-        {
-            name: 'Claudia Yamile Gomez Llanez',
-            id: '05096',
-            email: 'claudiaygomez@ufps.edu.co',
-            github: 'GitHub',
-            image: 'https://via.placeholder.com/300'
-        },
-        {
-            name: 'Claudia Yamile Gomez Llanez',
-            id: '05096',
-            email: 'claudiaygomez@ufps.edu.co',
-            github: 'GitHub',
-            image: 'https://via.placeholder.com/300'
-        },
-        {
-            name: 'Claudia Yamile Gomez Llanez',
-            id: '05096',
-            email: 'claudiaygomez@ufps.edu.co',
-            github: 'GitHub',
-            image: 'https://via.placeholder.com/300'
-        },
-        {
-            name: 'Claudia Yamile Gomez Llanez',
-            id: '05096',
-            email: 'claudiaygomez@ufps.edu.co',
-            github: 'GitHub',
-            image: 'https://via.placeholder.com/300'
-        },
-        {
-            name: 'Claudia Yamile Gomez Llanez',
-            id: '05096',
-            email: 'claudiaygomez@ufps.edu.co',
-            github: 'GitHub',
-            image: 'https://via.placeholder.com/300'
-        }
-    ];
+const studentsList = document.getElementById('studentsList');
+const template = document.getElementById('studentCardTemplate');
 
-    const studentsList = document.getElementById('studentsList');
-    const template = document.getElementById('studentCardTemplate');
+// Render students
+async function renderStudents() {
+    const students = await api.getStudents();
+    studentsList.innerHTML = '';
+    students.forEach(student => {
+        console.log(student);
+        const clone = template.content.cloneNode(true);
+        
+        clone.querySelector('.student-name').textContent = student.name;
+        
+        clone.querySelector('.student-id').textContent = `ID: ${student.code}`;
+        clone.querySelector('.student-email').textContent = student.email;
+        clone.querySelector('.student-image').src = student.photo;
 
-    // Render students
-    function renderStudents() {
-        studentsList.innerHTML = '';
-        students.forEach(student => {
-            console.log(student.name);
-            
-            const clone = template.content.cloneNode(true);
-            
-            clone.querySelector('.student-name').textContent = student.name;
-            /*
-            clone.querySelector('.student-id').textContent = `ID: ${student.id}`;
-            clone.querySelector('.student-email').textContent = student.email;
-            clone.querySelector('.student-image').src = student.image;
-            clone.querySelector('.github-link').href = `https://github.com/${student.github}`;
-            */
-            studentsList.appendChild(clone);
-            
+        clone.querySelector('.student-image').addEventListener('click', () => {
+            console.log('Image clicked');
         });
-    }
+        clone.querySelector('.github-link').href = `https://github.com/${student.github}`;
+        
+        studentsList.appendChild(clone);
+        
+    });
+}
 
-    // Initial render
-    renderStudents();
+const btnLoad = document.getElementById('loadStudent');
+const btnNew = document.getElementById('addNewStudent');
 
+
+
+btnLoad.addEventListener('click', async () => {
+    await renderStudents();
+});
+
+
+btnNew.addEventListener('click',  () => {
+    window.location.href = "https://www.google.com";
 });
